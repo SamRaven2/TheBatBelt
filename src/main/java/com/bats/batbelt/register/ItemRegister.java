b5ddb8ff745f31444a5ee5fdbbf30af8d34f7f40
SamRaven2 <samraven2@gmail.com>
2015-04-26 17:56:20 -0400

    Very little should have changed

--- src/main/java/com/bats/batbelt/register/ItemRegister.java
+++ src/main/java/com/bats/batbelt/register/ItemRegister.java
@@ -3,13 +3,15 @@ package com.bats.batbelt.register;
 import com.bats.batbelt.item.ItemCobbleGen;
 import com.bats.batbelt.reference.Settings;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 
 /**
  * Created by Bats on 4/26/2015.
  */
 public class ItemRegister
 {
-    public static ItemCobbleGen itemCobbleGen = new ItemCobbleGen();
+    public static Item itemCobbleGen = new ItemCobbleGen();
 
     public static void init()
     {
