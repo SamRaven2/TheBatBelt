714b8553a1381ce4f7e3ca90ebfcccba36259741
SamRaven2 <samraven2@gmail.com>
2015-04-26 02:24:38 -0400

    Yeah, lots of work

---
+++ Rebuild Workspace.bat
@@ -0,0 +1,1 @@
+gradlew clean --refresh-dependencies setupDecompWorkspace idea
---
+++ src/main/java/com/bats/batbelt/handler/FuelHandler.java
@@ -0,0 +1,39 @@
+package com.bats.batbelt.handler;
+
+import com.bats.batbelt.reference.Settings;
+import cpw.mods.fml.common.IFuelHandler;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+/**
+ * Created by Bats on 4/26/2015.
+ */
+public class FuelHandler implements IFuelHandler
+{
+    @Override
+    public int getBurnTime(ItemStack fuel)
+    {
+        int[] fuelOreIds = OreDictionary.getOreIDs(fuel);
+
+        if(fuelOreIds.length <= 0)
+            return 0;
+
+        if(Settings.CrossMod.fuelCoalDust)
+        {
+            int coalDustOreID = OreDictionary.getOreID("dustCoal");
+            for (int i : fuelOreIds)
+                if(i == coalDustOreID)
+                    return 200;
+        }
+
+        if(Settings.CrossMod.fuelCharcoalDust)
+        {
+            int charcoalDustOreID = OreDictionary.getOreID("dustCharcoal");
+            for (int i : fuelOreIds)
+                if(i == charcoalDustOreID)
+                    return 200;
+        }
+
+        return 0;
+    }
+}
