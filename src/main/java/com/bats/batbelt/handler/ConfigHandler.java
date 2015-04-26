e3956a24b4c68a12224774a2149ed969734c6799
SamRaven2 <samraven2@gmail.com>
2015-04-26 02:25:17 -0400

    Revert "Revert "Yeah, lots of work""
    This reverts commit 56525091d4db5c3c0c92ba4cd7e98764df432ac3.

---
+++ src/main/java/com/bats/batbelt/handler/ConfigHandler.java
@@ -0,0 +1,43 @@
+package com.bats.batbelt.handler;
+
+import com.bats.batbelt.reference.ModRef;
+import com.bats.batbelt.reference.Settings;
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+/**
+ * Created by Bats on 4/25/2015.
+ */
+public class ConfigHandler
+{
+    public static Configuration config;
+
+    public static void init(File configFile)
+    {
+        if (config == null)
+        {
+            config = new Configuration(configFile);
+            loadConfig();
+        }
+    }
+
+    private static void loadConfig()
+    {
+        Settings.CrossMod.fuelCharcoalDust = config.getBoolean("fuelCharcoalDust", Configuration.CATEGORY_GENERAL, true, "Does charcoal dust function as fuel?");
+        Settings.CrossMod.fuelCoalDust = config.getBoolean("fuelCoalDust", Configuration.CATEGORY_GENERAL, true, "Does coal dust function as fuel?");
+
+        Settings.Items.itemCobbleGen = config.getBoolean("itemCobbleGen", "Items", true, "Is the handheld cobble gen enabled?");
+        if (config.hasChanged())
+            config.save();
+    }
+
+    @SubscribeEvent
+    public void onConfigChangedEvent(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(ModRef.MOD_ID))
+            loadConfig();
+    }
+}
